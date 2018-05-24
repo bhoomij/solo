0f989aa91d66ba54316b36f2fa9c14e204136193
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2018-05-25 01:31:36 +0530

    update in team section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+/* ================================================
+                    Preloader
+=================================================== */
+
 // once full website is loaded remove preolaoder
-$(window).on("load", function() {
+$(window).on("load", function () {
     $("#status").fadeOut();
     $("#preloader").delay(350).fadeOut('slow');
+});
+
+/* ================================================
+                    Team
+=================================================== */
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        loop: true,
+        dots: false,
+        nav: true,
+        smartSpeed: 700,
+        autoplayHoverPause: true,
+        navText: ['<i class="fa fa-angle-left fa-2x"></i>', '<i class="fa fa-angle-right fa-2x"></i>']
+    });
 });
